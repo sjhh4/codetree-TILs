a2c6df11a20b482aa7a95c0fed63723806451e98
Hyoungrak Oh <58450279+sjhh4@users.noreply.github.com>
2024-01-31 20:56:28 +0900

    Title: 알파벳에 따른 평가, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240131/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+n = input()
+
+if n == 'S':
+    print('Superior')
+elif n == 'A':
+    print('Excellent')
+elif n == 'B':
+    print('Good')
+elif n == 'C':
+    print('Usually')
+elif n == 'D':
+    print('Effort')
+else:
+    print('Failure')
