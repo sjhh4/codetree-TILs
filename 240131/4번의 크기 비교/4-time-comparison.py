e30d13e30b2785f4d86031f9464ee02965557dbe
Hyoungrak Oh <58450279+sjhh4@users.noreply.github.com>
2024-01-31 22:26:28 +0900

    Title: 4번의 크기 비교, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240131/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,22 @@
+a = int(input())
+b, c, d, e = map(int, input().split())
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a>c:
+    print(1)
+else:
+    print(0)
+
+if a>d:
+    print(1)
+else:
+    print(0)
+
+if a>e:
+    print(1)
+else:
+    print(0)
