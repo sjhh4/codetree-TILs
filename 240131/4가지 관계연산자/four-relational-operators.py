29f83817bf94664312d1e228b54ca2840d57de9f
Hyoungrak Oh <58450279+sjhh4@users.noreply.github.com>
2024-01-31 22:21:49 +0900

    Title: 4가지 관계연산자, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240131/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+
+if a>=b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if b>=a:
+    print(1)
+else:
+    print(0)
+
+if b>a:
+    print(1)
+else:
+    print(0)
