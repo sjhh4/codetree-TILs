e8bd760d47004468eaa7f3c8cec91d1f7a2ac0eb
Hyoungrak Oh <58450279+sjhh4@users.noreply.github.com>
2024-02-09 19:15:02 +0900

    Title: 369 게임, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240209/369 게임/369-game.py
+++ 240209/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n + 1):
-    if i//100 ==3 or i//100==6 or i//100==9 or (i%100)//10==3 or (i%100)//10==6 or (i%100)//10==9 or i%10==3 or i%10==6 or i%10==9:
+    if i//100 ==3 or i//100==6 or i//100==9 or (i%100)//10==3 or (i%100)//10==6 or (i%100)//10==9 or i%10==3 or i%10==6 or i%10==9 or i%3 ==0:
         print(0, end = ' ')
     else:
         print(i, end = ' ')
